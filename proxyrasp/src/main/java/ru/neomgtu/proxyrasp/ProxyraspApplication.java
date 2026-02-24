3c48ce6edb2ca5149cc8c411303db93b591b4a0e
firecx <nikandrov.alekseix1@gmail.com>
2026-02-24 17:02:51 +0600

    весна пришла

---
+++ proxyrasp/src/main/java/ru/neomgtu/proxyrasp/ProxyraspApplication.java
@@ -0,0 +1,13 @@
+package ru.neomgtu.proxyrasp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyraspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyraspApplication.class, args);
+	}
+
+}
---
+++ proxyrasp/src/test/java/ru/neomgtu/proxyrasp/ProxyraspApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.neomgtu.proxyrasp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyraspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
