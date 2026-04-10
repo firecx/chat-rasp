4476e9270091ede1ccf02557cd7806878e1ca66a
Firecx <nikandrov.alekseix1@gmail.com>
2026-04-10 23:21:47 +0600

    switching to json

--- proxyrasp/src/main/java/ru/neomgtu/proxyrasp/interfaces/ExternalScheduleServer.java
+++ proxyrasp/src/main/java/ru/neomgtu/proxyrasp/interfaces/ExternalScheduleServer.java
@@ -1,17 +1,20 @@
 package ru.neomgtu.proxyrasp.interfaces;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
 
 import reactor.core.publisher.Mono;
+import ru.neomgtu.proxyrasp.dto.SubjectDto;
 
 @HttpExchange(url = "/api/schedule")
 public interface ExternalScheduleServer {
 
-    @GetExchange("/group/{groupId}.ics")
-    Mono<String> getScheduleByGroupId(
+    @GetExchange("/group/{groupId}")
+    Mono<List<SubjectDto>> getScheduleByGroupId(
             @PathVariable String groupId,
             @RequestParam String start,
             @RequestParam String finish,
--- proxyrasp/src/main/java/ru/neomgtu/proxyrasp/services/ScheduleService.java
+++ proxyrasp/src/main/java/ru/neomgtu/proxyrasp/services/ScheduleService.java
@@ -1,23 +1,21 @@
 package ru.neomgtu.proxyrasp.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
+import ru.neomgtu.proxyrasp.dto.SubjectDto;
 import ru.neomgtu.proxyrasp.interfaces.ExternalScheduleServer;
-import ru.neomgtu.proxyrasp.mapper.IcsToJsonVeventsMapper;
 
 @Service
 @RequiredArgsConstructor
 public class ScheduleService {
 
     private final ExternalScheduleServer externalScheduleServer;
-    private final IcsToJsonVeventsMapper icsToJsonVeventsMapper;
     
-
-    public Mono<String> getScheduleByGroupId(String groupId, String start, String finish, int lng) {
-        return icsToJsonVeventsMapper.convertToJson(
-            externalScheduleServer.getScheduleByGroupId(groupId, start, finish, lng)
-        );
+    public Mono<List<SubjectDto>> getScheduleByGroupId(String groupId, String start, String finish, int lng) {
+        return externalScheduleServer.getScheduleByGroupId(groupId, start, finish, lng);
     }
 }
